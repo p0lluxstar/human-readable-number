91357d6a7fe34e2716adbbe058bcff26c1972d22
global-p0lluxstar <global-web-registra@yandex.ru>
2023-01-12 00:17:44 +0300

    120123-0:17

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ module.exports = function toReadable (number) {
 
     if (number < 10){
         return arr0_9[number];
-    } else if (number < 20){
+    };
+
+    if (number < 20){
         return arr10_19[number - 10];
-    } else if (number < 100){
+    };
+
+    if (number < 100){
         let toStr = number.toString();
         let toNumStart = toStr.slice(0, -1);
         let toNumEnd = toStr.slice(1);
@@ -16,24 +20,27 @@ module.exports = function toReadable (number) {
             } else {
                 return `${arrX0[toNumStart - 2]} ${arr0_9[toNumEnd]}`
             };
-    } else if (number < 1000){
+    };
+
+    if (number < 1000){
         let toStr = number.toString();
         let toNumStartX00 = toStr.slice(0, 1);
         let toNumCenterX00 = toStr.slice(1, 2);
         let toNumEndX00 = toStr.slice(-1);
             if (toNumCenterX00 == 1){
                 return `${arr0_9[toNumStartX00]} hundred ${arr10_19[toNumEndX00]}`;
-            }
+            };
 
             if (toNumCenterX00 == 0 && toNumEndX00 == 0){
                 return `${arr0_9[toNumStartX00]} hundred`;
-            }
+            };
 
             if (toNumCenterX00 == 0){
                 return `${arr0_9[toNumStartX00]} hundred ${arr0_9[toNumEndX00]}`;
             } else if (toNumEndX00 == 0){
                 return `${arr0_9[toNumStartX00]} hundred ${arrX0[toNumCenterX00 - 2]}`;
-            } return `${arr0_9[toNumStartX00]} hundred ${arrX0[toNumCenterX00 - 2]} ${arr0_9[toNumEndX00]}`;
-    }
-    
-}
+            } else {
+                return `${arr0_9[toNumStartX00]} hundred ${arrX0[toNumCenterX00 - 2]} ${arr0_9[toNumEndX00]}`
+            };
+    };
+};
